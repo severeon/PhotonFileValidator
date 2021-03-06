73f70660d50d14c39462ea4bdc14f98c307b5598
Thomas Quick <severeon@gmail.com>
2021-03-06 16:31:50 -0600

    Removed gui stuff from code, it should be in the bytecode instead.
    Added keylistener for 'X' key in the EditDialog, which shows the layer above it

--- install/makeapp.sh
+++ install/makeapp.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-jar2app ../out/artifacts/PhotonFileValidator.jar -i img/icons/validator.icns -n "Photon File Validator" -v 2.1.0 -s 2.1.0 -c "(C) Copyright by Bonosoft" -j "-Xmx5g -Xms1g"
+jar2app ../out/artifacts/PhotonFileValidator.jar -i img/icons/validator.icns -n "Photon File Validator" -v 2.1.1 -s 2.1.1 -c "(C) Copyright by Bonosoft" -j "-Xmx5g -Xms1g"
 
